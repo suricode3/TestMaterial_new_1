738cacb8ed370c8f0c5773c4487a605c1c0208f4
suricode3 <suri.code3@gmail.com>
2017-03-22 21:50:58 +0530

    test variable added

--- app/src/main/java/com/example/callisto/testmaterial1/Main2Activity.java
+++ app/src/main/java/com/example/callisto/testmaterial1/Main2Activity.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 public class Main2Activity extends AppCompatActivity {
 
-
+    int testvariableforcommit=0;
     RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {

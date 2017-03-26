5e08258af78a11eec56e98d2cf47ade745259255
suricode3 <suri.code3@gmail.com>
2017-03-26 10:03:45 +0530

    Added test variable

--- app/src/main/java/com/example/callisto/testmaterial1/MainActivity.java
+++ app/src/main/java/com/example/callisto/testmaterial1/MainActivity.java
@@ -4,18 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.transition.Fade;
 import android.transition.TransitionManager;
-import android.transition.TransitionSet;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    int test1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
